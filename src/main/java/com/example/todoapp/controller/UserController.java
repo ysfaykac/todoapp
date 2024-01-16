2c41f521107fa5ef5148f4a5ac17f60e1afb6731
Yusuf Aykaç <ysfaykac@gmail.com>
2024-01-16 12:04:27 +0300

    Update UserController.java

--- src/main/java/com/example/todoapp/controller/UserController.java
+++ src/main/java/com/example/todoapp/controller/UserController.java
@@ -1,34 +1,35 @@
 package com.example.todoapp.controller;
 
-import com.example.todoapp.dto.role.CreateUserRoleRequest;
-import com.example.todoapp.dto.user.CreateUserDto;
-import com.example.todoapp.dto.user.UserDto;
+import com.example.todoapp.dto.auth.LoginDto;
+import com.example.todoapp.dto.auth.RegisterUserDto;
+import com.example.todoapp.dto.auth.TokenDto;
+import com.example.todoapp.services.AuthService;
 import com.example.todoapp.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    @GetMapping
-    public ResponseEntity<List<UserDto>> getUsers() {
-        return ResponseEntity.ok(userService.getUsers());
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> signIn(@RequestBody @Valid LoginDto loginDto) {
+        return ResponseEntity.ok(authService.login(loginDto));
     }
 
-    @PostMapping
-    public ResponseEntity<UserDto> saveUser(CreateUserDto userDto) {
-        return ResponseEntity.ok(userService.save(userDto));
-    }
+    @PostMapping("/register")
+    public ResponseEntity<?> signUp(@RequestBody @Valid RegisterUserDto data) {
+        userService.register(data);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
 
-    @PatchMapping
-    public ResponseEntity<?> addRoleToUser(@RequestBody CreateUserRoleRequest request) {
-        userService.addRoleToUser(request.getUsername(), request.getRoleName());
-        return ResponseEntity.ok().build();
     }
-
 }
