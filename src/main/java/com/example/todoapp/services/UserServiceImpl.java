9ae5bd14307b8015dd176de950b6cf3b793bdd5c
Yusuf Aykaç <ysfaykac@gmail.com>
2024-01-16 13:13:39 +0300

    Update UserServiceImpl.java

--- src/main/java/com/example/todoapp/services/UserServiceImpl.java
+++ src/main/java/com/example/todoapp/services/UserServiceImpl.java
@@ -1,66 +1,61 @@
 package com.example.todoapp.services;
 
-import com.example.todoapp.dto.role.CreateRoleDto;
-import com.example.todoapp.dto.role.RoleDto;
+import com.example.todoapp.dto.auth.RegisterUserDto;
 import com.example.todoapp.dto.user.CreateUserDto;
 import com.example.todoapp.dto.user.UserDto;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import com.example.todoapp.entity.User;
+import com.example.todoapp.entity.UserRole;
+import com.example.todoapp.exceptions.UserAlreadyExistsException;
 import com.example.todoapp.mapper.CustomMapper;
-import com.example.todoapp.repositories.RoleRepository;
-import com.example.todoapp.repositories.UserRepository;
+import com.example.todoapp.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
-@Transactional(readOnly = true)
-public class UserServiceImpl implements UserService
-{
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
     private final CustomMapper customMapper;
-    public final BCryptPasswordEncoder bCryptPasswordEncoder;
     @Override
-    @Transactional
     public UserDto save(CreateUserDto createUserDto) {
         User user = customMapper.createUserDtoToUser(createUserDto);
-        user.setPassword(bCryptPasswordEncoder.encode(createUserDto.getPassword()));
+        String encryptedPassword = new BCryptPasswordEncoder().encode(createUserDto.getPassword());
+        user.setPassword(encryptedPassword);
         return customMapper.userToUserDto(userRepository.save(user));
     }
 
     @Override
-    @Transactional
-    public RoleDto save(CreateRoleDto createRoleDto) {
-        var role = customMapper.createRoleDtoToRole(createRoleDto);
-        return customMapper.roleToRoleDto(roleRepository.save(role));
-    }
-
-    @Override
-    @Transactional
-    public void addRoleToUser(String username, String roleName) {
-        var user = userRepository.findByUsername(username);
-        var role = roleRepository.findByName(roleName);
-        user.getRoles().add(role);
+    public void addRoleToUser(String username, UserRole roleName) {
+        var user = userRepository.findUserByUsername(username);
+        user.setRole(roleName);
         userRepository.save(user);
     }
 
     @Override
     public UserDto getUser(String username) {
-        return customMapper.userToUserDto(userRepository.findByUsername(username));
-
+        return customMapper.userToUserDto(userRepository.findUserByUsername(username));
     }
-
     @Override
     public List<UserDto> getUsers() {
         return customMapper.userListToUserDtoList(userRepository.findAll());
+    }
+    @Override
+    public void register(RegisterUserDto data) {
+        var user= userRepository.findUserByUsername(data.username());
+        if(user!=null){
+            throw new UserAlreadyExistsException();
+        }
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.name(),data.username(), encryptedPassword, data.role());
+        userRepository.save(newUser);
     }
 
     @Override
@@ -68,7 +63,19 @@ public class UserServiceImpl implements UserService
         var user = userRepository.findByUsername(username);
         if(user==null)
             throw new UsernameNotFoundException("User not found");
-       return user;
-
+        return user;
     }
+
+    @Override
+    public UserDto getAuthentication() {
+        var auth= (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        var user=userRepository.findUserByUsername(auth.getUsername());
+        return customMapper.userToUserDto(user);
+    }
+
+    @Override
+    public User findUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
 }
