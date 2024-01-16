72f0df706892f1f3661fe5e02abb10df37ac060d
Yusuf Aykaç <ysfaykac@gmail.com>
2024-01-16 13:13:29 +0300

    Update UserService.java

--- src/main/java/com/example/todoapp/services/UserService.java
+++ src/main/java/com/example/todoapp/services/UserService.java
@@ -1,21 +1,21 @@
 package com.example.todoapp.services;
 
-import com.example.todoapp.dto.role.CreateRoleDto;
-import com.example.todoapp.dto.role.RoleDto;
+import com.example.todoapp.dto.auth.RegisterUserDto;
 import com.example.todoapp.dto.user.CreateUserDto;
 import com.example.todoapp.dto.user.UserDto;
-import com.example.todoapp.entity.Role;
 import com.example.todoapp.entity.User;
+import com.example.todoapp.entity.UserRole;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
 public interface UserService extends UserDetailsService {
-
     UserDto save(CreateUserDto user);
-    RoleDto save(CreateRoleDto role);
-    void addRoleToUser(String username, String roleName);
+    void addRoleToUser(String username, UserRole roleName);
     UserDto getUser(String username);
     List<UserDto> getUsers();
+    void register(RegisterUserDto data);
+    UserDto getAuthentication();
+    User findUserById(Long id);
 
 }
