8859ff333a0bbaa65312828bc33a197244dea89d
Yusuf Aykaç <ysfaykac@gmail.com>
2024-01-16 11:50:17 +0300

    Create JwtFilter.java

---
+++ JwtFilter.java
@@ -0,0 +1,40 @@
+package com.example.todoapp.config;
+
+import com.example.todoapp.repository.UserRepository;
+import com.example.todoapp.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.getToken(request);
+        if (token != null) {
+            var username = jwtService.validateToken(token);
+            var user = userRepository.findByUsername(username);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+    private String getToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null)
+            return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
