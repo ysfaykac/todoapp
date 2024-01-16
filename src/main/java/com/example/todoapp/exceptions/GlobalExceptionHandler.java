c18f4076edbaaab24f733abb961e76f231a75f71
Yusuf Aykaç <ysfaykac@gmail.com>
2024-01-16 12:26:47 +0300

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/example/todoapp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.example.todoapp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Map<String, List<String>>> handleGeneralExceptions(Exception ex) {
+        List<String> errors = List.of(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(errorsMap(errors));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public final ResponseEntity<Map<String, List<String>>> handleRuntimeExceptions(RuntimeException ex) {
+        List<String> errors = List.of(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(errorsMap(errors));
+    }
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Map<String, List<String>>> handleJwtErrors(UserAlreadyExistsException ex) {
+        List<String> errors = List.of(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorsMap(errors));
+    }
+    @ExceptionHandler(JwtInvalidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleJwtErrors(JwtInvalidException ex) {
+        List<String> errors = List.of(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorsMap(errors));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, List<String>>> handleBadCredentialsError(BadCredentialsException ex) {
+
+        List<String> errors = List.of(ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorsMap(errors));
+    }
+    private Map<String, List<String>> errorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
+    }
+}
